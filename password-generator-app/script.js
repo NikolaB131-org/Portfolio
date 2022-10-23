e98f501dfa0a963fe07df28ca77e58aa92d11502
Николай <Kolya.Balancev@yandex.ru>
2022-10-23 16:51:04 +0300

    Finished JS logic

--- password-generator-app/script.js
+++ password-generator-app/script.js
@@ -1,16 +1,34 @@
 // Slider
-let sliderNumber = document.querySelector('.slider-title__number');
+let passwordLength = document.querySelector('.slider-length__number');
 document.querySelector('.slider-wrapper__slider').oninput = function() {
   let percent = (this.value - 1) * 5.25;
   this.style.background = `linear-gradient(90deg, #fc751a ${percent}%, #1f1c1b ${percent}%)`;
-  sliderNumber.innerText = this.value;
+  passwordLength.innerText = this.value;
 }
 
 // Strength level
 let checkCounter = 0;
 let strengthBlockChildren = [...document.querySelector('.strength-block__strength-level-wrapper').children]
 let strengthBlockText = document.querySelector('.strength-block__strength-level');
+let includeUppercase = false;
+let includeLowercase = false;
+let includeNumbers = false;
+let includeSymbols = false;
 document.querySelector('.settings-block__list').oninput = function(event) {
+  switch(event.target.id) {
+    case 'checkbox-includeUppercase':
+      includeUppercase = event.target.checked;
+      break;
+    case 'checkbox-includeLowercase':
+      includeLowercase = event.target.checked;
+      break;
+    case 'checkbox-includeNumbers':
+      includeNumbers = event.target.checked;
+      break;
+    case 'checkbox-includeSymbols':
+      includeSymbols = event.target.checked;
+      break;
+  }
   if (event.target.checked) {
     checkCounter++;
   } else {
@@ -45,4 +63,29 @@ document.querySelector('.settings-block__list').oninput = function(event) {
 }
 
 // Generator
-let availableCharacters = [];
+let password = document.querySelector('.password-block__password');
+document.querySelector('.settings-block__generate-button').onclick = function() {
+  let uppercaseCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+  let lowercaseCharacters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  let numbersCharacters = '0123456789'.split('');
+  let symbolsCharacters = '!@#$%^&*()-_+=;:,./?`~[]{}^\'"<>~.'.split('');
+  let availableCharacters = [
+    ...includeUppercase ? uppercaseCharacters : [],
+    ...includeLowercase ? lowercaseCharacters : [],
+    ...includeNumbers ? numbersCharacters : [],
+    ...includeSymbols ? symbolsCharacters : []
+  ];
+  let generatedPassword = '';
+  if (availableCharacters.length) { // at least one checkbox checked
+    for (let i = 0; i < +passwordLength.innerText; i++) {
+      let charIndex = Math.floor(Math.random() * availableCharacters.length);
+      generatedPassword += availableCharacters[charIndex];
+    }
+  }
+  password.value = generatedPassword;
+}
+
+// Copy button
+document.querySelector('.password-block__copy-button').onclick = function() {
+  navigator.clipboard.writeText(password.value);
+}
