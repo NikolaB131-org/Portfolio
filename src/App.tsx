3b6d17499a4cef058d5127e0358f18bb08657461
Николай <Kolya.Balancev@yandex.ru>
2023-01-21 16:06:26 +0300

    Intro page done

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import arrowImg from './assets/arrow.svg'
+import starImg from './assets/star.png'
+import s from './App.module.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+      <div className={s.line1}></div>
+      <div className={s.line2}></div>
+      <img className={s.star} src={starImg} />
+      <section className={s.intro}>
+        <div className={s['intro__greeting-start']}>&lt;Привет&gt; Меня зовут</div>
+        <h1 className={s.intro__name}>Николай Баланцев</h1>
+        <h3 className={s.intro__specialization}>Front-end разработчик</h3>
+        <p className={s.intro__description}>Я front-end разработчик. Учусь в университете МИРЭА. Мне 20 лет. Я ем француские булки с пряностями и люблю кофе, но я пью цикорий. Обожаю матча латте, это очень вкусно.</p>
+        <div className={s['intro__greeting-end']}>&lt;/Привет&gt;</div>
+      </section>
+      <img className={s.arrow} src={arrowImg} alt="Пролистать вниз" />
+      {/* <section className={s.skills}>
+        <h2>Навыки</h2>
+      </section> */}
+    </>
   )
 }
 
