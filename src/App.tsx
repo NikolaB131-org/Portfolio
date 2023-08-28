d60873495c7a6a18d091698e4c4fef265f997aa6
Nikolai <Nikolai.Balantsev@yandex.ru>
2023-08-28 03:03:55 +0300

    Add 2 projects

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useEffect, useRef } from 'react';
 import { ProjectCard } from './components/ProjectCard';
 import starPng from './assets/star.png';
 import arrowSvg from './assets/arrow.svg';
+import armageddon_2023_screenshot from './assets/projects_screenshots/Armageddon-2023.png';
+import message_editor_screenshot from './assets/projects_screenshots/Message-editor.png';
 import soundwave_frontend_screenshot from './assets/projects_screenshots/Soundwave-frontend.jpg';
 import avito_internship_project_screenshot from './assets/projects_screenshots/Avito-internship-project_1280x720.png';
 import password_generator_app_screenshot from './assets/projects_screenshots/Password-generator-app_1280x720.jpg';
@@ -49,46 +51,58 @@ export function App() {
           <div className={styles['intro__greeting-start']}>&lt;Привет&gt; Меня зовут</div>
           <h1 className={styles.intro__name}>Николай Баланцев</h1>
           <h3 className={styles.intro__specialization}>Front-end разработчик</h3>
-          <p className={styles.intro__description}>Мне 20 лет. Обучаюсь в РТУ МИРЭА на 4 курсе по специальности "Программная инженерия".<br />До поступления понял что больше всего интересует веб-разработка, поэтому в свободное время изучал этот удивительный мир.<br />Прошел стажировку в команде международного такси в Яндексе.</p>
+          <p className={styles.intro__description}>Мне 20 лет. Обучаюсь в РТУ МИРЭА на 4 курсе по специальности "Программная инженерия".<br />В свободное время изучал удивительный мир веб-разработки.<br />Прошел стажировку в команде международного такси в Яндексе.</p>
           <div className={styles['intro__greeting-end']}>&lt;/Привет&gt;</div>
         </section>
       </div>
 
-      <div className={styles['projects-section']}>
-        <section className={styles.projects}>
-          <h2 className={styles.projects__header}>Проекты</h2>
-          <div className={styles.projects__container}>
-            <ProjectCard
-              previewImg={soundwave_frontend_screenshot}
-              linkGitHub="https://github.com/serguun42/SoundWave/tree/master/frontend"
-              header="Фронтенд для сервиса музыки">
-              Проект для университета, делался на коленке за 3 дня :) Бэкенд делал другой разработчик
-            </ProjectCard>
-            <ProjectCard
-              previewImg={avito_internship_project_screenshot}
-              linkGitHub="https://github.com/NikolaB131/Avito-internship-project"
-              header="Клон Hacker News">
-              Тестовое задание для отбора на стажировку в Авито. Клиент запрашивает информацию с локального сервера, а тот уже запрашивает с API Hacker News
-            </ProjectCard>
-            <ProjectCard
-              previewImg={password_generator_app_screenshot}
-              linkDemo="https://nikolab131.github.io/Password-generator-app/"
-              linkGitHub="https://github.com/NikolaB131/Password-generator-app"
-              header="Генератор паролей">
-              Простой генератор паролей, свёрстан на HTML+CSS, логика реализована на чистом JS<br />
-              Адаптивность присутствует
-            </ProjectCard>
-            <ProjectCard
-              previewImg={product_preview_card_component_screenshot}
-              linkDemo="https://nikolab131.github.io/Product-preview-card-component/"
-              linkGitHub="https://github.com/NikolaB131/Product-preview-card-component"
-              header="Карточка предварительного просмотра продукта">
-              Карточка свёрстана на HTML+CSS.<br />
-              Адаптивность присутствует
-            </ProjectCard>
-          </div>
-        </section>
-      </div>
+      <section className={styles.projects}>
+        <h2 className={styles.projects__header}>Проекты</h2>
+        <div className={styles.projects__container}>
+          <ProjectCard
+            previewImg={armageddon_2023_screenshot}
+            linkDemo="https://armageddon.nikolab131.xyz/"
+            linkGitHub="https://github.com/NikolaB131-org/Armageddon-2023"
+            header="Armageddon 2023">
+            Онлайн-сервис по мониторингу и уничтожению опасных астероидов на основе данных API NASA.<br />Next.js, Адаптивная вёрстка
+          </ProjectCard>
+          <ProjectCard
+            previewImg={message_editor_screenshot}
+            linkDemo="https://message-editor.nikolab131.xyz/"
+            linkGitHub="https://github.com/NikolaB131-org/Message-editor"
+            header="Message editor">
+            Редактор сообщений с поддержкой переменных и гибких вложенных условий. Данные хранится в виде дерева и сериализуются в JSON
+          </ProjectCard>
+          <ProjectCard
+            previewImg={soundwave_frontend_screenshot}
+            linkGitHub="https://github.com/serguun42/SoundWave/tree/master/frontend"
+            header="Фронтенд для сервиса музыки">
+            Проект для университета, делался на коленке за 3 дня :) Бэкенд делал другой разработчик.<br />Redux toolkit, React Router
+          </ProjectCard>
+          <ProjectCard
+            previewImg={avito_internship_project_screenshot}
+            linkGitHub="https://github.com/NikolaB131/Avito-internship-project"
+            header="Клон Hacker News">
+            Тестовое задание для отбора на стажировку в Авито. Клиент запрашивает информацию с локального сервера, а тот уже запрашивает с API Hacker News
+          </ProjectCard>
+          <ProjectCard
+            previewImg={password_generator_app_screenshot}
+            linkDemo="https://nikolab131.github.io/Password-generator-app/"
+            linkGitHub="https://github.com/NikolaB131/Password-generator-app"
+            header="Генератор паролей">
+            Простой генератор паролей, свёрстан на HTML+CSS, логика реализована на чистом JS<br />
+            Адаптивная вёрстка
+          </ProjectCard>
+          <ProjectCard
+            previewImg={product_preview_card_component_screenshot}
+            linkDemo="https://nikolab131.github.io/Product-preview-card-component/"
+            linkGitHub="https://github.com/NikolaB131/Product-preview-card-component"
+            header="Карточка предварительного просмотра продукта">
+            Карточка свёрстана на HTML+CSS.<br />
+            Адаптивная вёрстка
+          </ProjectCard>
+        </div>
+      </section>
     </>
   );
 }
